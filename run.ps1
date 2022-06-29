a680d612e8b5299147a07504480fddeb35d719b1
Joakim Leed <90039793+OthelloEngineer@users.noreply.github.com>
2022-06-29 23:15:19 +0200

    Add run.ps1

---
+++ run.ps1
@@ -0,0 +1,22 @@
+param(
+    [Parameter(Mandatory)]
+    [string] $GITHUB_TOKEN
+)
+
+scoop install terraform
+Write-host "Terraform Installed"
+
+Set-Item -Path env:GITHUB_TOKEN -Value $GITHUB_TOKEN
+Write-host "Environmental variable set"
+
+terraform init 
+Write-host "terraform initialised"
+
+terraform fmt
+write-host "syntax formated"
+
+terraform apply -target github_repository.my_repository -target github_branch.init_branch -auto-approve
+write-host "repository and terraform branch made"
+
+terraform apply -auto-approve
+Write-Host "Github repository created"
